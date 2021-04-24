d3059cc0db5e699c50b78647b97c58e25988ddc1
Izdag <myigogoshka@mail.ru>
2021-04-24 22:47:32 +0300

    выполнила дз3

--- main.js
+++ main.js
@@ -1,75 +1,41 @@
-
 // №1
-// Вслучае использования префиксного инкремента в выражениях,
-// в функции alert выводится уже увеличенное значение переменной.
-// При использовании же постфикстного инкремента, в функцию alert выводится текущее значение переменной.
+// let i = 2;
+// mainCycle:
+// while (i < 100) {
+//     i++
+//     let j = 2;
+//     while (j < i) {
+//         if (i % j == 0) continue mainCycle
+//         j++
+//     }
+//     alert(i)
+// }
+
 
 
 // №2
-// Ответ 5
+// let priceArr = [85, 95, 700, 63, 92, 489, 1000]
+// function countBasketPrice(priceArr) {
+//     let pricesSum = 0
+//     for (let i = 0; i < priceArr.length; i++) {
+//         pricesSum += priceArr[i]
+//     }
+//     return pricesSum
+// }
+// const totalPrice = countBasketPrice(priceArr)
+// alert(totalPrice)
+
 
 
 // №3
-// let a = -90;
-// let b = 26;
-// if (a >= 0 && b >= 0) {
-//     alert(Math.abs(a - b));
-// } else if (a <= 0 && b <= 0) {
-//     alert(a * b);
-// } else if (a != b) {
-//     alert(a + b);
+// for (let i = 0; i <= 9; alert(i++)) {
 // }
 
 
 // №4
-// let stringNumber = prompt('Ввeдите номер')
-// let num = parseInt(stringNumber)
-// let numbers = ''
-// for (let count = num; count <= 15; count++) {
-//     numbers = numbers + count
+// let star = ''
+// for (let i = 1; i <= 20; i++) {
+//     star = star + '*'
+//     console.log(star)
 // }
-// alert(numbers)
-
-
-// №5
-function sum(a, b) {
-    return a + b;
-}
-// let c = sum(73, 98)
-// alert(c);
-
-function subtraction(a, b) {
-    return a - b;
-}
-// let c = subtraction(54, 18)
-// alert(c);
-
-function multiplication(a, b) {
-    return a * b;
-}
-// let c = multiplication(7, 10)
-// alert(c);
-
-function division(a, b) {
-    return a / b;
-}
-// let c = division(500, 5)
-// alert(c);
-
-// №6
-function mathOperation(arg1, arg2, operation) {
-    switch (operation) {
-        case 'sum':
-            return sum(arg1, arg2)
-        case 'subtraction':
-            return subtraction(arg1, arg2)
-        case 'multiplication':
-            return multiplication(arg1, arg2)
-        case 'division':
-            return division(arg1, arg2)
-        default:
-            break;
-    }
-}
-
-alert(mathOperation(16, 7, 'multiplication'))
+// console.log(star.length)
